e9137e0653f4bbb39d07d8a3f0983a9ba3e8be13
小范 <65595489+1996fkl@users.noreply.github.com>
2020-11-26 22:38:14 +0800

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,10 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-<<<<<<< HEAD
-=======
-		System.out.println("Hello World");
-		System.out.println("Hello World");
->>>>>>> origin/branch2
+	System.out.println("Hello World");
+	System.out.println("Hello World");
     }
 }
